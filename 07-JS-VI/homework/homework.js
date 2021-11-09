0020d8d0d74dc63303a16b130276f201941fc006
Day Torres <88747375+day2555@users.noreply.github.com>
2021-11-08 21:06:57 -0600

    JS07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,20 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return nombre [0].toUpperCase() + nombre.slice(1);
+  return nombre[0].toUpperCase() + nombre.slice(1); // slice corta desde la posición 1.
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-  cb();
+  cb(); // Como sabemos que es una función la invocamos como una función cualquiera.
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-  return cb(n1, n2);
+  return cb(n1, n2); //Retornamos el cb con los dos argumentos.
 }
 
 function sumarArray(numeros, cb) {
@@ -25,45 +25,68 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-  let suma = numeros.reduce(function(acc, elemento) {
-    return acc + elemento;
-  }, 0); 
-  cb(suma); 
+  var sumaReduce = numeros.reduce(function(acumulador,elemento){
+    return (acumulador + elemento); 
+  }, 0); // El 0 es e segundo parametro en la invocación del reduce. Es el valor en el cual se va a inicializar el acc.
+  cb(sumaReduce); 
 }
+//Reduce permite reducir todos los elementos de un arreglo a partir de una función que le indiquemos.
+/* De la manera clásica sería: 
+var suma = 0; 
+  for(var i = 0; i < numeros.length; i++){
+    suma += numeros[i]
+  }
+  cb(suma);
+*/
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  array.forEach(function(elemento, indice) {
-    cb(elemento);
-  });  
-} 
-
+  array.forEach(function(elemento, indice) { 
+    cb(elemento, indice)
+  });
+}
+/* De la manera clásica en un for:
+for (var i = 0; i < array.length; i++) {
+  cb(array[i])
+}
+*/
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  let otroArray = array.map(function(elemento) {
-    return cb(elemento); 
+  var nuevoArray = array.map(function(elemento, indice){
+    return cb(elemento) // Dentro del map siempre debemos retornar algo
   });
-  return otroArray;
+  return nuevoArray; 
 }
+//map permite crear un nuevo arreglo con cada uno de los elementos del arreglo anterior que se le puede agregar otro método a cada uno incluso.
+//El arreglo original se queda como estaba.
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  let filtrarArray = []; 
-  for(var i = 0; i < array.length; i++) {
-    if(array[i] [0] === 'a') {
-      filtrarArray.push(array [i])
-    }
-  }
-  return filtrarArray;
+  var nuevoArray = array.filter(function(elemento){
+    return elemento[0] === "a" 
+  },0);
+  return nuevoArray; 
 }
+// Filter permite crear un nuevo arreglo SOLO con los elementos del original que cumplan cierta condición.
+//También funciona con strings.
+/* De la manera clásica: 
+var nuevoArray = [];
+for(let i = 0; i<array.length; i++) {
+  if(array[i][0] === "a") {
+    nuevoArray.push(array[i])
+  }
+}
+return nuevoArray;
+*/
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
